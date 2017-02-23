3bfbd7b9956a1a7438b8bf4f911adcc5fd67592c
nklthesis <luca.giacolini.16@ucl.ac.uk>
2017-02-23 23:44:39 +0000

    Angry-protective behavior

---
+++ test_6/behaviors.ino
@@ -0,0 +1,42 @@
+void randomSweep()
+{
+  //swtiching in between rest and moving somewhere randomly
+  switch (random(0,1)){
+    
+  case 0:
+  servo.write(random(0, 100));
+  servo1.write(random(30, 60));
+  delay(150);
+  servo.write(random(100, 180));
+  servo1.write(random(60, 80));
+  delay(150);
+  break;
+
+  case 1:
+  delay(400); 
+  break;
+  }
+}
+
+void panning()
+{
+  servo.write(0);
+  delay(100);
+  servo.write(180);
+  delay(500);
+}
+
+void defense()
+{
+  servo.write(100);
+  servo1.write(90);
+  delay(300);
+  servo1.write(70);
+  delay(100);
+  servo1.write(90);
+  delay(100);
+    servo1.write(70);
+  delay(100);
+  servo1.write(25);
+  servo.write(112);
+}
---
+++ test_6/test_6.ino
@@ -0,0 +1,72 @@
+// Sketch successfully tested on Arduino Uno Rev3.
+
+// LICENCE
+// Luca Giacolini
+// www.lucagiacolini.com
+// University College of London, Interactive Architecture Lab
+// Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+
+#include <Servo.h>
+Servo servo;           // Define bottom servo 0-180
+Servo servo1;         // Define top servo 0-180
+
+int sensorPin = A3;   //sensor pin
+int med;
+int avVal = 0;
+
+int counter1 = 0;
+int counter2 = 0;
+
+bool state;         // current state of the button
+int lastState = 0;     // previous state of the button
+
+bool pos;
+
+void setup() {
+  
+  delay(1000);
+  servo.attach(2);
+  servo1.attach(4); 
+  //set initial position   
+  servo.write(112);
+  servo1.write(25);
+  
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+  //calibration routine
+  sensorSetup();
+}
+ 
+void loop() {
+  
+  sensorRead();
+  if(avVal < (med-10))
+  {
+    state = true;
+  }
+  else if(avVal > (med+5))
+  {
+    state = false;
+  }  
+
+  if (state != lastState) 
+  {
+    if (state == true) 
+    {
+      counter1++;
+
+    } 
+    else 
+    {
+      counter2++;
+    }
+  }
+
+  lastState = state;
+
+  if(counter1 == 4 && counter2 == 4){
+    defense();
+    counter1 = 0;
+    counter2 = 0;
+    }
+}
